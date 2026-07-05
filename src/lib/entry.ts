a2961b2e13936f43aa7732a44706baa58e70375c
aryaveersr <me.aryaveer@gmail.com>
2026-07-05 23:33:04 +0530

    ensure svelte's reactivity is retained by mutating instead of reassigning entry objects

--- src/lib/entry.ts
+++ src/lib/entry.ts
@@ -43,30 +43,25 @@ export function destroyEntry(entry: Entry) {
 }
 
 export function uploadEntry(entry: Entry) {
-	if (entry.kind != 'base') return;
+	if ((entry as BaseEntry).kind != 'base') return;
 
 	const xhr = new XMLHttpRequest();
 	const form = new FormData();
 
 	form.set('file', entry.file);
-	entry = {
-		...entry,
-		kind: 'processing',
-		progress: 0
-	} satisfies ProcessingEntry;
+
+	(entry as ProcessingEntry).kind = 'processing';
+	(entry as ProcessingEntry).progress = 0;
 
 	xhr.upload.addEventListener('progress', (ev) => {
-		if (ev.lengthComputable && entry.kind == 'processing') {
-			entry.progress = Math.round((ev.loaded / ev.total) * 100);
+		if (ev.lengthComputable && (entry as ProcessingEntry).kind == 'processing') {
+			(entry as ProcessingEntry).progress = Math.round((ev.loaded / ev.total) * 100);
 		}
 	});
 
 	xhr.addEventListener('load', () => {
-		entry = {
-			...entry,
-			kind: 'processed',
-			out: URL.createObjectURL(xhr.response)
-		} satisfies ProcessedEntry;
+		(entry as ProcessedEntry).kind = 'processed';
+		(entry as ProcessedEntry).out = URL.createObjectURL(xhr.response);
 	});
 
 	xhr.responseType = 'blob';
