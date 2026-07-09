337ba4fc5d8e2f6b5b8738e06095966d2d4b7af9
aryaveersr <me.aryaveer@gmail.com>
2026-07-09 17:16:09 +0530

    settings overlay with header

--- src/lib/components/Settings/Settings.svelte
+++ src/lib/components/Settings/Settings.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { fade } from 'svelte/transition';
-
+	import Button from '../ui/Button.svelte';
+	import { X } from '@lucide/svelte';
 	let isOpen = $derived(page.url.searchParams.has('settings'));
 
 	$effect(() => console.log(isOpen));
@@ -10,7 +11,12 @@
 {#if isOpen}
 	<section transition:fade={{ duration: 100 }}>
 		<div>
-			<p>Hello, world!</p>
+			<header>
+				<h3>Settings</h3>
+				<Button kind="ghost" size="md" onclick={() => window.history.back()}>
+					<X />
+				</Button>
+			</header>
 		</div>
 	</section>
 {/if}
@@ -40,5 +46,29 @@
 		background-color: var(--bg-surface);
 		border-radius: var(--radius-lg);
 		border: 1px solid var(--border);
+	}
+
+	h3 {
+		/* Font settings */
+		font-weight: 400;
+		font-size: var(--text-default);
+
+		/* Spacing */
+		padding: 0.5rem 1.25rem;
+	}
+
+	header {
+		/* Layout */
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		/* Appearance */
+		border-bottom: 1px solid var(--border-muted);
+
+		& :global(button) {
+			border-radius: 0px;
+			border-top-right-radius: var(--radius-lg);
+		}
 	}
 </style>
