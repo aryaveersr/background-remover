14133d6233898576683b85eef352286d80f7c451
aryaveersr <me.aryaveer@gmail.com>
2026-07-09 18:31:13 +0530

    basic toggle button

--- src/lib/components/Settings/Settings.svelte
+++ src/lib/components/Settings/Settings.svelte
@@ -3,26 +3,40 @@
 	import { fade } from 'svelte/transition';
 	import Button from '../ui/Button.svelte';
 	import { X } from '@lucide/svelte';
+	import Toggle from '../ui/Toggle.svelte';
 	let isOpen = $derived(page.url.searchParams.has('settings'));
 
 	$effect(() => console.log(isOpen));
 </script>
 
 {#if isOpen}
-	<section transition:fade={{ duration: 100 }}>
-		<div>
+	<div class="container" transition:fade={{ duration: 100 }}>
+		<section>
 			<header>
 				<h3>Settings</h3>
 				<Button kind="ghost" size="md" onclick={() => window.history.back()}>
 					<X />
 				</Button>
 			</header>
-		</div>
-	</section>
+			<div class="section">
+				<h4>User Interface</h4>
+				<ul>
+					<li>
+						<span>Color scheme</span>
+						<Button kind="subtle">tEst</Button>
+					</li>
+					<li>
+						<span>Auto download when done</span>
+						<Toggle initial={false} onchange={(v) => console.log(v)} />
+					</li>
+				</ul>
+			</div>
+		</section>
+	</div>
 {/if}
 
 <style>
-	section {
+	.container {
 		/* Cover the screen */
 		position: absolute;
 		z-index: 99;
@@ -37,10 +51,10 @@
 		place-items: center;
 	}
 
-	div {
+	section {
 		/* Size */
-		width: 75%;
-		height: 75%;
+		width: 90%;
+		height: 80%;
 
 		/* Appearance */
 		background-color: var(--bg-surface);
@@ -69,6 +83,75 @@
 		& :global(button) {
 			border-radius: 0px;
 			border-top-right-radius: var(--radius-lg);
+		}
+	}
+
+	.section {
+		/* Layout */
+		display: flex;
+		flex-direction: column;
+
+		/* Spacing */
+		padding: 1.25rem;
+		gap: 1.5rem;
+	}
+
+	h4 {
+		/* Font settings */
+		font-weight: 400;
+		font-size: var(--text-default);
+
+		/* Layout (for underline) */
+		display: flex;
+		flex-direction: column;
+
+		/* Spacing */
+		gap: 0.25rem;
+	}
+
+	h4::after {
+		content: '';
+		width: 50%;
+		margin-right: auto;
+		border-bottom: 1px solid var(--border);
+	}
+
+	ul {
+		/* Reset styles */
+		list-style: none;
+
+		/* Layout */
+		display: flex;
+		flex-direction: column;
+
+		/* Spacing */
+		gap: 1rem;
+	}
+
+	li {
+		/* Fill width */
+		width: 100%;
+
+		/* Layout */
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	li > span {
+		width: 50%;
+	}
+
+	@media (min-width: 900px) {
+		section {
+			/* Overlay size */
+			width: 75%;
+			height: 75%;
+		}
+
+		.section {
+			/* Spacing */
+			padding: 4rem 8rem;
 		}
 	}
 </style>
---
+++ src/lib/components/ui/Toggle.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	interface Props {
+		initial?: boolean;
+		onchange?: (value: boolean) => void;
+	}
+
+	let { initial = false, onchange = () => {} }: Props = $props();
+
+	// svelte-ignore state_referenced_locally
+	let on = $state(initial);
+</script>
+
+<label class:on>
+	<input type="checkbox" class="visually-hidden" bind:checked={on} onchange={() => onchange(on)} />
+	<span>
+		{#if on}
+			ON
+		{:else}
+			OFF
+		{/if}
+	</span>
+</label>
+
+<style>
+	label {
+		/* Size */
+		width: 4rem;
+
+		/* Appearance */
+		background-color: var(--bg-muted);
+		border-radius: var(--radius-md);
+
+		/* Spacing */
+		padding: 4px;
+
+		/* Interaction */
+		cursor: pointer;
+		user-select: none;
+
+		&.on {
+			background-color: var(--primary-solid);
+			color: var(--fg);
+		}
+	}
+
+	span {
+		display: block;
+
+		/* Size */
+		width: 2rem;
+		font-size: var(--text-xs);
+
+		/* Spacing */
+		padding: 0.25rem;
+
+		/* Appearance */
+		background-color: var(--bg-surface);
+		border-radius: var(--radius-md);
+		text-align: center;
+
+		.on & {
+			background-color: var(--primary-contrast);
+			transform: translateX(1.5rem);
+		}
+	}
+</style>
